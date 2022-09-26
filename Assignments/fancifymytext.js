21345af06f5f5f23bbabcbf9dc6bcab14a49584a
bdam98 <62578649+bdam98@users.noreply.github.com>
2022-09-26 02:34:25 -0700

    Add files via upload

---
+++ Assignments/fancifymytext.js
@@ -0,0 +1,28 @@
+function makeBig(){
+    document.getElementById("textarea").style.fontSize = "24pt";
+}
+
+function fancy(radio){
+    var textarea = document.getElementById("textarea");
+    textarea.style.fontWeight = "bold";
+    textarea.style.color = "blue";
+    textarea.style.textDecoration = "underline";
+}
+
+function boring(radio){
+    var textarea = document.getElementById("textarea");
+    textarea.style.fontWeight = "normal";
+    textarea.style.color = "black";
+    textarea.style.textDecoration = "none";
+}
+
+function moo(){
+    var textarea = document.getElementById("textarea").value;
+    var text = textarea.toUpperCase();
+    var split = text.split(".");
+    var str = split.join("-Moo.");
+    document.getElementById("textarea").value = str;
+}
+
+
+
